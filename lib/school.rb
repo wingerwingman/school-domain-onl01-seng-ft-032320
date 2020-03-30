8cc7803ab6b4cc348a2e89ac76046f520e015c7c
Justin Winger <justinwinger4@gmail.com>
2020-03-30 18:34:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,10 +15,14 @@ class School
       @roster[grade] << student_name
     end
   end
-  def grade(level)
-    @roster.each do |x, y|
-      @roster[x] = y.sort 
-      end 
-      @roster
+
+    def grade(number)
+      @roster[number]
+    end
+    
+    def sort 
+      @roster.each do |grade, name|
+        @roster[grade] = name.sort
+      end
     end
 end
